faf912dac861d235f44d5863829587aa8db0ad4d
Taylor Thurman <taylor.thurman@protonmail.com>
2023-09-05 20:12:09 -0500

    day 1

--- day-1-printing-start/main.py
+++ day-1-printing-start/main.py
@@ -1,16 +1,40 @@
-# This is a sample Python script.
+# Exercise 1
+print('''Day 1 - Python Print Function
+The function is declared like this:
+print('what to print')''')
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Exercise 2
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
 
+# Exercise 3
+name = input("What is your name? ")
+print(len(name))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Exercise 4
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
 
+####################################
+#Write your code below this line 👇
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+temp = a
+a = b
+b = temp
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+# Exercise 5
+print("Welcome to the Band Name Generator.")
+city = input("What's name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + pet)
